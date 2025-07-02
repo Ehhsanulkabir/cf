0f600d3902488e917cf6bf38c3e24e2196dcd9b4
EHHSANUL KABIR <csed8138@gmail.com>
2025-07-02 21:28:51 +0600

    M. Capital or Small or Digit

---
+++ M. Capital or Small or Digit.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char ch;
+    cin>> ch;
+
+    if(ch >= 'A' and ch <= 'Z')
+        cout << "ALPHA\nIS CAPITAL"<<endl;
+        else if(ch >= 'a' and ch<= 'z')
+        cout<<"ALPHA\nIS SMALL"<<endl;
+        else
+        cout<<"IS DIGIT"<<endl;
+        return 0;
+    }
+
